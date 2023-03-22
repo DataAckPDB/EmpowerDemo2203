c3c4d1c54958a193fbe644fd802972a99bb81337
Jan-Klaas Kesteloot <janklaas.kesteloot@skyline.be>
2023-03-22 14:15:53 +0100

    Add project files.

---
+++ EmpowerDemo2203_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo2203_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo2203_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("97DB07B6-8F47-438B-A473-CCB8DC8754A4")]
+[assembly: AssemblyVersion("1.0.0.0")]
